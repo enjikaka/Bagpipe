7dd91575fe2114cb52752f50e760c775c0954c6f
Jeremy Karlsson <karlsson@jeremy.se>
2015-09-25 02:05:35 +0200

    Added gulpfile with Sass

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/* jshint globalstrict: true */
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  gulp.src('src/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/css/*.css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./src/scss/*.scss', ['sass']);
+});
